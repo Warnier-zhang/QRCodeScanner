277538f25ab7333aab963b7ff8085a9c37c6a447
Warnier-zhang <warnier.zhang@gmail.com>
2016-07-18 16:35:35 +0800

    feature(JQRCodeFrame): Support preview instantly

---
+++ src/main/java/org/warnier/zhang/qrcode/JQRCodeFrame.java
@@ -0,0 +1,45 @@
+package org.warnier.zhang.qrcode;
+
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamPanel;
+import com.github.sarxos.webcam.WebcamResolution;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class JQRCodeFrame extends JFrame {
+    private Webcam webcam;
+    private JTextArea textArea;
+
+    public JQRCodeFrame() {
+        super();
+        webcam = Webcam.getWebcams().get(0);
+        webcam.setViewSize(WebcamResolution.QVGA.getSize());
+        JPanel cameraContainer = new WebcamPanel(webcam);
+        add(cameraContainer, BorderLayout.NORTH);
+
+        textArea = new JTextArea();
+        textArea.setEnabled(false);
+        textArea.setSize(400, 400);
+        add(textArea, BorderLayout.CENTER);
+
+        JButton button = new JButton("关闭摄像头");
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                webcam.close();
+            }
+        });
+        add(button, BorderLayout.SOUTH);
+    }
+
+    public void setText(final String text) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                textArea.setText(text);
+            }
+        });
+    }
+}
+
