034cafde5144b837d1adc9e505ac9c8613ad5cc1
Warnier-zhang <warnier.zhang@gmail.com>
2016-07-18 14:48:14 +0800

    Adjust the strategy of capturing image

--- src/main/java/org/warnier/zhang/qrcode/QRCodeScanner.java
+++ src/main/java/org/warnier/zhang/qrcode/QRCodeScanner.java
@@ -5,8 +5,15 @@ import com.google.zxing.*;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.qrcode.QRCodeReader;
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  * QRCodeScanner can scan and decode QR codes with the build-in camera on laptop PC.
@@ -19,14 +26,29 @@ public class QRCodeScanner {
     }
 
     public String scan() {
-        // Open camera on the computer;
+        Result result;
         Webcam camera = Webcam.getDefault();
         camera.open();
-        BufferedImage image = camera.getImage();
-        return decode(image);
+        do {
+            if (!camera.isOpen()) {
+                camera.open();
+            }
+            BufferedImage image;
+            if ((image = camera.getImage()) == null) {
+                continue;
+            }
+            makeLog(image);
+            result = decode(image);
+            if (result != null) {
+                break;
+            }
+            delayMillis(1750);
+        } while (true);
+        playSound("sound.mp3");
+        return result.getText();
     }
 
-    private String decode(BufferedImage image) {
+    private Result decode(BufferedImage image) {
         // A standard interface for abstracting different bitmaps;
         LuminanceSource source = new BufferedImageLuminanceSource(image);
         // It is designed for high frequency images of barcodes with black data on white backgrounds.
@@ -42,6 +64,37 @@ public class QRCodeScanner {
         } catch (FormatException e) {
             e.printStackTrace();
         }
-        return result == null ? "二维码失效！" : result.getText();
+        return result;
+    }
+
+    private void makeLog(BufferedImage image) {
+        try {
+            ImageIO.write(image, "PNG", new File("scanner.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void playSound(String mp3) {
+        FileInputStream stream = null;
+        try {
+            stream = new FileInputStream(mp3);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            Player player = new Player(stream);
+            player.play();
+        } catch (JavaLayerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void delayMillis(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
