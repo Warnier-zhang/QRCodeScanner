b249a6515529ac82612aac8cb5973db17f155c79
Warnier-zhang <warnier.zhang@gmail.com>
2016-07-18 15:05:12 +0800

    feature(project): Add new testcase

--- src/main/java/org/warnier/zhang/qrcode/Console.java
+++ src/main/java/org/warnier/zhang/qrcode/Console.java
@@ -1,12 +1,9 @@
 package org.warnier.zhang.qrcode;
 
-/**
- * Created by Warnier-zhang on 2016/7/16.
- */
 public class Console {
-    public static final void main(String[] args) {
+    public static void main(String[] args) {
 //        QRCodeGenerator generator = new QRCodeGenerator();
-//        generator.generate("test!");
+//        generator.generate("上海心源计算机技术有限公司！");
         QRCodeScanner scanner = new QRCodeScanner();
         System.out.println(scanner.scan());
     }
