a95b9e64a884a5fbdeac489b347d28d4b996dd76
Warnier-zhang <warnier.zhang@gmail.com>
2016-07-18 16:36:28 +0800

    fix(Console): Add new testcase

--- src/main/java/org/warnier/zhang/qrcode/Console.java
+++ src/main/java/org/warnier/zhang/qrcode/Console.java
@@ -1,10 +1,18 @@
 package org.warnier.zhang.qrcode;
 
+import javax.swing.*;
+
 public class Console {
     public static void main(String[] args) {
 //        QRCodeGenerator generator = new QRCodeGenerator();
 //        generator.generate("上海心源计算机技术有限公司！");
-        QRCodeScanner scanner = new QRCodeScanner();
-        System.out.println(scanner.scan());
+//        QRCodeScanner scanner = new QRCodeScanner();
+//        System.out.println(scanner.scan());
+        JQRCodeFrame frame = new JQRCodeFrame();
+        frame.setTitle("QR Code");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.pack();
+        frame.setText(new QRCodeScanner().scan());
     }
 }
--- src/main/java/org/warnier/zhang/qrcode/QRCodeScanner.java
+++ src/main/java/org/warnier/zhang/qrcode/QRCodeScanner.java
@@ -43,7 +43,6 @@ public class QRCodeScanner {
             delayMillis(1750);
         } while (true);
         playSound("beep.mp3");
-
         return result.getText();
     }
 
