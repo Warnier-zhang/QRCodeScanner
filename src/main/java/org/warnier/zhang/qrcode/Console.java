9603622ead86159fdaaf2c06efb59a98acdf66fe
Warnier-zhang <warnier.zhang@gmail.com>
2016-07-25 11:42:22 +0800

    fix(Console): New testcase

--- src/main/java/org/warnier/zhang/qrcode/Console.java
+++ src/main/java/org/warnier/zhang/qrcode/Console.java
@@ -2,8 +2,11 @@ package org.warnier.zhang.qrcode;
 
 public class Console {
     public static void main(String[] args) {
+//        QRCodeGenerator generator = new QRCodeGenerator();
+//        generator.generate("Warnier-zhang!");
         // 益达口香糖：6923450657713
         BarcodeGenerator generator = new BarcodeGenerator("3450", "65771");
         generator.generate();
+
     }
 }
